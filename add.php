336e1c31e9039c943c961993c4e745d526a4a7d9
optimusSAAD <saadbinomar998@gmail.com>
2019-06-25 17:47:46 +0600

    Add task For Todo List

---
+++ add.php
@@ -0,0 +1,12 @@
+<?php
+include 'db.php';
+if (isset($_POST['send'])) {
+$name = htmlspecialchars($_POST['task']);
+$sql = "insert into task (name) values('$name')";
+$val = $db->query($sql);
+if($val){
+	header('location: index.php');
+	
+}
+}
+?>
